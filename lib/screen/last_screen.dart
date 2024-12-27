3335e4812d2a0814ea8261b9b20b6f70ce43c69b
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-12-28 00:14:36 +0900

    유튜브 플레이어 적용

--- lib/screen/last_screen.dart
+++ lib/screen/last_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class LastScreen extends StatefulWidget {
   const LastScreen({super.key});
@@ -8,13 +9,34 @@ class LastScreen extends StatefulWidget {
 }
 
 class _LastScreenState extends State<LastScreen> {
+  late YoutubePlayerController _youtubePlayerController;
+  @override
+  void initState() {
+    super.initState();
+
+    const videoId = 'UU7ca4Ooi4o';
+
+    _youtubePlayerController = YoutubePlayerController(
+      initialVideoId: videoId,
+      flags: const YoutubePlayerFlags(autoPlay: true),
+    );
+  }
+
+  @override
+  void dispose() {
+    _youtubePlayerController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('last'),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: YoutubePlayer(
+          controller: _youtubePlayerController,
+          showVideoProgressIndicator: true,
+        ),
       ),
     );
   }
